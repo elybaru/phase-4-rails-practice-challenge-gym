90773899dc23bd82d9a47fa30e97c9a625d83d1d
Ely Karr <elykarr@gmail.com>
2022-04-05 10:51:12 -0700

    commit tree c639a4bcee064436cac6d60be8ab95a423eca8f4

---
+++ app/controllers/memberships_controller.rb
@@ -0,0 +1,16 @@
+class MembershipsController < ApplicationController
+    validates 
+
+    def create
+        membership = Membership.create!(membership_params)
+
+    end
+
+    private
+
+    def membership_params
+        params.permit(:gym_id, :client_id, :charge)
+    end
+
+
+end
